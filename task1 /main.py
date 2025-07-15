316d28f79b77428fb6c58a270b7229874e8477ac
Shaf.Abdin <shaf2446@gmail.com>
2025-07-15 19:28:52 +0100

    Create main.py

---
+++ task1 /main.py
@@ -0,0 +1,109 @@
+import json, unittest, datetime
+from msvcrt import kbhit
+
+with open("./data-1.json", "r") as f:
+    jsonData1 = json.load(f)
+with open("./data-2.json", "r") as f:
+    jsonData2 = json.load(f)
+with open("./data-result.json", "r") as f:
+    jsonExpectedResult = json.load(f)
+
+
+def convertFromFormat1(jsonObject):
+    # IMPLEMENT: Conversion From Type 1
+
+    location__ = jsonObject.get['location'].split('/')
+
+    converted_file = {
+        'deviceID' : jsonObject[0],
+        'deviceType' : jsonObject[1],
+        'timestamp' : jsonObject[2],
+
+        'location' : {
+            'country' : location__[0],
+            'city' : location__[1],
+            'area' : location__[2],
+            'factory' : location__[3],
+            'section' : location__[4]
+        },
+
+        'data' : {
+            'status' : jsonObject['operationStatus'],
+            'temperature' : jsonObject['temp']
+        }
+    }
+
+    return converted_file
+
+
+def convertFromFormat2(jsonObject):
+    # IMPLEMENT: Conversion From Type 1
+  
+    # inspiration from https://stackoverflow.com/questions/60442518/python-3-convert-iso-8601-to-milliseconds/60443033#60443033
+    millisec = datetime.datetime(2021, 6, 23, 10, 57, 17).timestamp()
+
+
+    converted_file = {
+        'deviceID' : jsonObject['device']['id'],
+        'deviceType' : jsonObject['device']['type'],
+
+        'timestamp' : millisec,
+
+        'location' : {
+            'country' : jsonObject['country'],
+            'city' : jsonObject['city'],
+            'area' : jsonObject['area'],
+            'factory' : jsonObject['factory'],
+            'section' : jsonData1['section-1']
+
+        },
+
+        'data' : {
+            'status' : jsonObject['data']['status'],
+            'temperature' : jsonObject['data']['healthy']
+
+        }
+
+    }
+    return converted_file
+
+
+def main(jsonObject):
+    result = {}
+
+    if (jsonObject.get('device') == None):
+        result = convertFromFormat1(jsonObject)
+    else:
+        result = convertFromFormat2(jsonObject)
+
+    return result
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_sanity(self):
+        result = json.loads(json.dumps(jsonExpectedResult))
+        self.assertEqual(
+            result,
+            jsonExpectedResult
+        )
+
+    def test_dataType1(self):
+        result = main(jsonData1)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 1 failed'
+        )
+
+    def test_dataType2(self):
+        result = main(jsonData2)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 2 failed'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
